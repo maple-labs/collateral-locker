2dc7c60ac50a6729b746275cda4eea711d3c4c35
Michael De Luca <michael.deluca@circle-free.com>
2021-07-22 22:31:27 -0400

    fix: Tests and CI

--- contracts/test/CollateralLockerFactory.t.sol
+++ contracts/test/CollateralLockerFactory.t.sol
@@ -2,33 +2,65 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
-import { TestUtil } from "../../../../test/TestUtil.sol";
+import { DSTest } from "../../modules/ds-test/src/test.sol";
+import { ERC20 }  from "../../modules/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 import { ICollateralLocker } from "../interfaces/ICollateralLocker.sol";
 
-contract CollateralLockerFactoryTest is TestUtil {
+import { CollateralLockerFactory } from "../CollateralLockerFactory.sol";
 
-    function setUp() public {
-        setUpGlobals();
-        setUpTokens();
-        createCollateralLockerFactory();
-        createBorrower();
+contract MintableToken is ERC20 {
+
+    constructor (string memory name, string memory symbol) ERC20(name, symbol) public {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
     }
 
+}
+
+contract CollateralLockerOwner {
+
+    function collateralLockerFactory_newLocker(address factory, address token) external returns (address) {
+        return address(ICollateralLocker(CollateralLockerFactory(factory).newLocker(token)));
+    }
+
+    function try_collateralLockerFactory_newLocker(address factory, address token) external returns (bool ok, address locker) {
+        bytes memory data;
+        (ok, data) = factory.call(abi.encodeWithSignature("newLocker(address)", token));
+        (locker) = abi.decode(data, (address));
+    }
+
+    function collateralLocker_pull(address locker, address destination, uint256 amount) external {
+        ICollateralLocker(locker).pull(destination, amount);
+    }
+
+    function try_collateralLocker_pull(address locker, address destination, uint256 amount) external returns (bool ok) {
+        (ok,) = locker.call(abi.encodeWithSignature("pull(address,uint256)", destination, amount));
+    }
+
+}
+
+contract CollateralLockerFactoryTest is DSTest {
+
     function test_newLocker() public {
-        ICollateralLocker cl  = ICollateralLocker(clFactory.newLocker(USDC));
+        CollateralLockerFactory factory = new CollateralLockerFactory();
+        MintableToken token = new MintableToken("TKN", "TKN");
+        CollateralLockerOwner owner = new CollateralLockerOwner();
 
-        // Validate the storage of clfactory.
-        assertEq(clFactory.owner(address(cl)), address(this), "Invalid owner");
-        assertTrue(clFactory.isLocker(address(cl)));
+        ICollateralLocker locker = ICollateralLocker(owner.collateralLockerFactory_newLocker(address(factory), address(token)));
 
-        // Validate the storage of cl.
-        assertEq(cl.loan(), address(this),            "Incorrect loan address");
-        assertEq(address(cl.collateralAsset()), USDC, "Incorrect address of collateral asset");
+        // Validate the storage of factory.
+        assertEq(factory.owner(address(locker)), address(owner), "Invalid owner");
+        assertTrue(factory.isLocker(address(locker)), "Invalid isLocker");
 
-        // Assert that no one can access CollateralLocker funds
-        mint("USDC", address(cl),  500 * USD);
-        assertTrue(!bob.try_pull(address(cl), address(bob), 10));
+        // Validate the storage of locker.
+        assertEq(locker.loan(), address(owner),                     "Incorrect loan address");
+        assertEq(address(locker.collateralAsset()), address(token), "Incorrect address of collateral asset");
+
+        // Assert that only the CollateralLocker owner can access funds
+        token.mint(address(locker), 1);
+        assertTrue(owner.try_collateralLocker_pull(address(locker), address(owner), 1), "Pull failed");
     }
 
 }
