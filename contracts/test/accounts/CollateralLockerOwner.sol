cc73db0cba769c91397c1aafb4674622d7f7fb39
Michael De Luca <michael.deluca@circle-free.com>
2021-07-22 22:31:27 -0400

    fix: modules, tests, and CI

---
+++ contracts/test/accounts/CollateralLockerOwner.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import { ICollateralLocker } from "../../interfaces/ICollateralLocker.sol";
+
+import { CollateralLockerFactory } from "../../CollateralLockerFactory.sol";
+
+contract CollateralLockerOwner {
+
+    function collateralLockerFactory_newLocker(address factory, address token) external returns (address) {
+        return address(ICollateralLocker(CollateralLockerFactory(factory).newLocker(token)));
+    }
+
+    function try_collateralLockerFactory_newLocker(address factory, address token) external returns (bool ok) {
+        (ok,) = factory.call(abi.encodeWithSignature("newLocker(address)", token));
+    }
+
+    function collateralLocker_pull(address locker, address destination, uint256 amount) external {
+        ICollateralLocker(locker).pull(destination, amount);
+    }
+
+    function try_collateralLocker_pull(address locker, address destination, uint256 amount) external returns (bool ok) {
+        (ok,) = locker.call(abi.encodeWithSignature("pull(address,uint256)", destination, amount));
+    }
+
+}
